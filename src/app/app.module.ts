ec8f77355edaa76771c6deb529705dd0de5930f9
Mutagah <kihahumutagah@gmail.com>
2024-03-17 12:02:24 +0300

    :rocket:feat: Addition of a service for handling data retrieval.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+/*Services imports */
+import { UsersService } from './users.service';
+
+/*Component imports */
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,17 +15,20 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, UsersComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     NoopAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  constructor(private httpClient: HttpClient) {}
+
+  usersUrl = "https://fakestoreapi.com/users"
+  
+  getAllUsers(): Observable<any> {
+    return this.httpClient.get(this.usersUrl);
+  }
+}
