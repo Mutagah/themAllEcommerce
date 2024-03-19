fb23a543d99578b02c55390100a8379b029630df
Mutagah <kihahumutagah@gmail.com>
2024-03-19 15:27:40 +0300

    :construction:feat: User Form validations.

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -16,8 +16,20 @@ export class UserFormComponent implements OnInit {
       lastName: ['', Validators.required],
       userName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+      password: ['', Validators.required],
+      latitude: ['', Validators.required],
+      longitude: ['', Validators.required],
+      city: ['', Validators.required],
+      street: ['', Validators.required],
+      number: ['', Validators.required],
+      zipCode: ['', Validators.required],
     });
   }
 
   submitForm() {}
+
+  resetForm(){
+    this.myReactiveForm.reset()
+  }
 }
