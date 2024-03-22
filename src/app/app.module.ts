143884ffca3c5443afb9a0790ac4291af5244eac
Mutagah <kihahumutagah@gmail.com>
2024-03-22 13:14:30 +0300

    :lipstick:feat:Empty cart styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
+
+/*Component imports */
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
-import { ProductComponent } from './product/product.component';
-import { HeaderComponent } from './header/header.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { CategoriesComponent } from './categories/categories.component';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatDialogModule } from '@angular/material/dialog';
-import { CreateProductComponent } from './create-product/create-product.component';
-import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { DeleteConfirmationComponent } from './delete-confirmation/delete-confirmation.component';
 import { CartComponent } from './cart/cart.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { DeleteConfirmationComponent } from './delete-confirmation/delete-confirmation.component';
+
+/*Angular imports */
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -50,7 +54,7 @@ import { CartComponent } from './cart/cart.component';
     MatDialogModule,
     MatIconModule,
     ReactiveFormsModule,
-    MatSnackBarModule 
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
