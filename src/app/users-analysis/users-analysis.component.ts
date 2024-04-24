b620f9520f62ae8a96195dd44ea9001d600f5005
Mutagah <kihahumutagah@gmail.com>
2024-04-24 12:20:20 +0300

    :lipstick:feat: Rendering of analytics component.

--- src/app/users-analysis/users-analysis.component.ts
+++ src/app/users-analysis/users-analysis.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-users-analysis',
   templateUrl: './users-analysis.component.html',
-  styleUrls: ['./users-analysis.component.css']
+  styleUrls: ['./users-analysis.component.css'],
 })
-export class UsersAnalysisComponent {
-
-}
+export class UsersAnalysisComponent {}
